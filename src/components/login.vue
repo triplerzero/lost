05bf2f21e09b0c9f8f46de8ace8741912982d02e
tripler <1339350185@qq.com>
2019-01-14 15:54:18 +0800

    新建登录页

---
+++ src/components/login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="login">
+      <el-input suffix-icon="el-icon-edit" placeholder="请输入用户名" v-model="user"></el-input>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Login from '@/components/login'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect:'login'
+    },
+    {
+      path:'/login',
+      name:'Lgoin',
+      component:Login
     }
   ]
 })
