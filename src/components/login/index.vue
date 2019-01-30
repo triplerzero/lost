18ceac1c8b20039a641fccb51716c6b1278617a4
tripler <1339350185@qq.com>
2019-01-30 14:58:15 +0800

    响应式

--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="login">
-    <el-input suffix-icon="el-icon-edit" placeholder="请输入学号" v-model="user"></el-input>
-    <el-input suffix-icon placeholder="请输入密码" v-model="pwd" :type="eyeOpen?'text':'password'" @keyup.enter.native="handleLogin">
-      <i
-        :class="eyeOpen?'el-input__icon iconfont icon-kaiyan':'el-input__icon iconfont icon-biyan'"
-        slot="suffix"
-        @click="handleEye"
-      ></i>
-    </el-input>
-    <el-button type="primary" class="loginBtn" @click='handleLogin'>登录</el-button>
-    <el-button type="danger" class="loginBtn" @click="handleClear">清空</el-button>
-    <el-radio v-model="radio" label="1">学生</el-radio>
-    <el-radio v-model="radio" label="2">管理员</el-radio>
+    <el-row :gutter="8" justify="center" type='flex'>
+      <el-col :xs="24" :sm="12" :md="12" :lg="8" :xl="8">
+        <el-input suffix-icon="el-icon-edit" placeholder="请输入学号" v-model="user"></el-input>
+        <el-input
+          suffix-icon
+          placeholder="请输入密码"
+          v-model="pwd"
+          :type="eyeOpen?'text':'password'"
+          @keyup.enter.native="handleLogin"
+        >
+          <i
+            :class="eyeOpen?'el-input__icon iconfont icon-kaiyan':'el-input__icon iconfont icon-biyan'"
+            slot="suffix"
+            @click="handleEye"
+          ></i>
+        </el-input>
+        <el-button type="primary" class="loginBtn" @click="handleLogin">登录</el-button>
+        <el-button type="danger" class="loginBtn" @click="handleClear">清空</el-button>
+        <el-radio v-model="radio" label="1">学生</el-radio>
+        <el-radio v-model="radio" label="2">管理员</el-radio>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -30,17 +40,17 @@ export default {
   methods: {
     //眼睛开关
     handleEye() {
-      this.eyeOpen = !this.eyeOpen
+      this.eyeOpen = !this.eyeOpen;
     },
     //清除数据
     handleClear() {
-      this.user = ""
-      this.pwd = ""
+      this.user = "";
+      this.pwd = "";
     },
     //登录
     handleLogin() {
-      let userId = this.user
-      let pwd = this.pwd
+      let userId = this.user;
+      let pwd = this.pwd;
       //用户登录
       if (this.radio == "1") {
         axios.post("/user/login", { userId, pwd }).then(res => {
@@ -104,5 +114,21 @@ i {
     margin-left: 0;
     margin-bottom: 20px;
   }
+}
+.el-col {
+  border-radius: 4px;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
 }
 </style>
